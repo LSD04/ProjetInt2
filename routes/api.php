86153bdb5706b859aa2f617ff1d6072015d4424a
LSD04 <sarah.lamini1@gmail.com>
2024-03-12 16:09:27 -0400

    connexion

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccessController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthApiController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DemandesInscriptionsController;
 use App\Http\Controllers\EntreeSortiesController;
+use App\Http\Controllers\EntreeSortiesApiController;
 use App\Http\Controllers\LocalsController;
 use App\Http\Controllers\PermissionAccessController;
 use App\Http\Controllers\UserLocalsController;
@@ -25,22 +27,22 @@ use App\Http\Controllers\UtilisateursController;
 
 
 // Route pour la page d'accueil
-Route::get('/', function () {
-    return view('login');
+// Route::get('/', function () {
+//     return view('login');
 
-});
+// });
 
-// Route pour page d'accueil Admin
+
 
 
 // Route pour la gestion d'accès
-Route::get('/gestionAccess', [AccessController::class, 'index'])->name('gestionAccess');
+//Route::get('/gestionAccess', [AccessController::class, 'index'])->name('gestionAccess');
 
 // Route pour l'historique
-Route::get('/entree-sortie', [EntreeSortiesController::class, 'index'])->name('entreeSortie.index');
+Route::get('/entree-sortie_API', [EntreeSortiesApiController::class, 'index'])->name('entreeSortie_API.index');
 
 //Afficher le profil d'un utilisateur
-Route::get('/utilisateurs/{id}', [UtilisateursController::class, 'show'])->name('utilisateurs.show');
+//Route::get('/utilisateurs/{id}', [UtilisateursController::class, 'show'])->name('utilisateurs.show');
 
 
 
@@ -49,33 +51,33 @@ Route::get('/utilisateurs/{id}', [UtilisateursController::class, 'show'])->name(
 
 
 // Route pour afficher la page DemandesInscription.blade.php  Appliquer le middleware 'auth' pour protéger la route et utiliser le contrôleur pour la logique
-Route::get('/demandesinscription', [DemandesInscriptionsController::class, 'index'])->name('demandesinscription.index');
+//Route::get('/demandesinscription', [DemandesInscriptionsController::class, 'index'])->name('demandesinscription.index');
 
 
 // Route pour afficher les détails d'une demande d'inscription spécifique
-Route::get('/demandesinscription/{id}', [DemandesInscriptionsController::class, 'show'])->name('demandesinscription.show');
+//Route::get('/demandesinscription/{id}', [DemandesInscriptionsController::class, 'show'])->name('demandesinscription.show');
 
 
 // Route pour supprimer une demande d'inscription spécifique (si nécessaire)
-Route::delete('/demandesinscription/{id}', [DemandesInscriptionsController::class, 'destroy'])->name('demandesinscription.destroy');
+//Route::delete('/demandesinscription/{id}', [DemandesInscriptionsController::class, 'destroy'])->name('demandesinscription.destroy');
 
 
 // Met à jour une demande d'inscription spécifique
-Route::patch('/demandesinscription/{id}', [DemandesInscriptionsController::class, 'update'])->name('demandesinscription.update');
+//Route::patch('/demandesinscription/{id}', [DemandesInscriptionsController::class, 'update'])->name('demandesinscription.update');
 
 // Rétirer l,accès à tous les utilisateurs
-Route::patch('/utilisateurs/{id}/retirer-acces', [UtilisateursController::class, 'retirerAcces'])->name('utilisateurs.retirerAcces');
-Route::post('/utilisateurs/retirer-acces-tous', [UtilisateursController::class, 'retirerAccesTous'])->name('utilisateurs.retirerAccesTous');
+//Route::patch('/utilisateurs/{id}/retirer-acces', [UtilisateursController::class, 'retirerAcces'])->name('utilisateurs.retirerAcces');
+//Route::post('/utilisateurs/retirer-acces-tous', [UtilisateursController::class, 'retirerAccesTous'])->name('utilisateurs.retirerAccesTous');
 
 
 
 // Afficher demandes approuvées
-Route::get('/demandes-approuvees', [DemandesInscriptionsController::class, 'demandesApprouvees'])->name('demandes.approuvees');
+//Route::get('/demandes-approuvees', [DemandesInscriptionsController::class, 'demandesApprouvees'])->name('demandes.approuvees');
 
 
 //Routes Connexion
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+//Route::get('/login', [AuthApiController::class, 'showLoginForm'])->name('login');
+Route::post('/loginUtilisateur', [AuthApiController::class, 'login']);
 
 // Déconnexion
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+//Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
