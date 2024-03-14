9ff6ddebcb8f1879d0f2700b11c31c670aa5618e
Daouda22 <daoudatchanya1999@gmail.com>
2024-03-14 13:43:02 -0400

    ok
    ok

--- resources/views/entreeSortie.blade.php
+++ resources/views/entreeSortie.blade.php
@@ -4,37 +4,37 @@
 <div class="container">
     <h1>Historique des entrées et sorties</h1>
 
-      <!-- Formulaire de Recherche -->
-      <form action="{{ route('entreeSortie.index') }}" method="GET" class="mb-4">
-        <div class="input-group mb-3">
-            <label for="date" class="input-group-text">Filtrer par date:</label>
+    <!-- Formulaire de Recherche -->
+    <div class="mb-3">
+        <form action="{{ route('entreeSortie.index') }}" method="GET" class="input-group">
             <input type="date" class="form-control" id="date" name="date" value="{{ request('date') }}">
-            <button type="submit" class="btn btn-primary" style="margin-left: 8px;">Rechercher</button>
-        </div>
-    </form>
+            <button type="submit" class="btn btn-dark" style="margin-left: 8px;">Rechercher</button>
+        </form>
+    </div>
     
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Nom de l'utilisateur</th>
-                <th>Local</th>
-                <th>Date et heure d'entrée</th>
-                <th>Date et heure de sortie</th>
-                <th>Jour de la semaine</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($historique as $entreeSortie)
-            <tr>
-               
-                <td>{{ optional($entreeSortie->user)->nom }}</td>
-                <td>{{ $entreeSortie->local->nom }}</td> <!-- Assurez-vous que la relation 'local' est bien définie -->
-                <td>{{ $entreeSortie->date_et_heure_entree }}</td>
-                <td>{{ $entreeSortie->date_et_heure_sortie }}</td>
-                <td>{{ $entreeSortie->jour_de_la_semaine }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nom de l'utilisateur</th>
+                    <th>Local</th>
+                    <th>Date et heure d'entrée</th>
+                    <th>Date et heure de sortie</th>
+                    <th>Jour de la semaine</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($historique as $entreeSortie)
+                <tr>
+                    <td>{{ optional($entreeSortie->user)->nom }}</td>
+                    <td>{{ $entreeSortie->local->nom }}</td>
+                    <td>{{ $entreeSortie->date_et_heure_entree }}</td>
+                    <td>{{ $entreeSortie->date_et_heure_sortie }}</td>
+                    <td>{{ $entreeSortie->jour_de_la_semaine }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
